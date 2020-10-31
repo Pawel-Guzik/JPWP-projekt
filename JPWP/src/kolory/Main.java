b8cbb76c1694323eecbd6c9edee853aedd4a7d7f
Pawel-Guzik <73741090+Pawel-Guzik@users.noreply.github.com>
2020-10-31 20:22:55 +0100

    Add files via upload

---
+++ JPWP/src/kolory/Main.java
@@ -0,0 +1,19 @@
+package kolory;
+
+
+public class Main
+{
+
+
+    public static void main(String[] args)
+    {
+
+        System.out.println("hello world");
+        Window game_window = new Window();
+        game_window.setVisible(true);
+        game_window.menu();
+    }
+
+
+
+}
