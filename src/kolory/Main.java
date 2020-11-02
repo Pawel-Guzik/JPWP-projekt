e096b6ffb4ef04243d4dd96179a8bb1ba84a8c46
Pawel Guzik <pawel.guzik21@gmail.com>
2020-11-02 16:47:24 +0100

    W razie czego

--- src/kolory/Main.java
+++ src/kolory/Main.java
@@ -8,10 +8,12 @@ public class Main
     public static void main(String[] args)
     {
 
-        System.out.println("hello world");
+
         Window game_window = new Window();
         game_window.setVisible(true);
         game_window.menu();
+
+
     }
 
 
--- src/kolory/Window.java
+++ src/kolory/Window.java
@@ -3,40 +3,53 @@ package kolory;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 
 public class Window extends JFrame implements ActionListener
 {
+    private JButton start;
+    private JPanel p;
+//    private JPanel g;
+
+
     public Window()
     {
         setSize(1280,1024);
         setTitle("GRA W KOLORY");
         setLayout(new BorderLayout());
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
     }
+
+
+
 
     public void menu()              // funkcja dodaje przycisk menu
     {
 
-        JButton start = new JButton("PLAY");
+        start = new JButton("PLAY");
         start.setBackground(Color.CYAN);
-
         start.setPreferredSize(new Dimension(200,100));
-        JPanel p = new JPanel();
+        start.addActionListener(this);
 
+        p = new JPanel();
         p.add(start);
         p.setBackground(Color.BLACK);
-
         this.add(p);
-
     }
 
-
-
     @Override
-    public void actionPerformed(ActionEvent arg0)
-
-
+    public void actionPerformed(ActionEvent e) {
+        Object zrodlo = e.getSource();
+        if(zrodlo == start)
+        {
+            System.out.println("kutas");
+            p.setVisible(false);
+        }else {
+            System.out.println("siki");
+        }
+    }
 
 
 
